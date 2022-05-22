4b0029d39dbc09a7ec9aa27340e5d184d4279fb1
Pavel <57864071+DrDick@users.noreply.github.com>
2022-05-22 22:00:08 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+$appsConfig = array (
+    '419f09a694685212cc4a01ff2e629a36' =>
+        array (
+            'BOT_ID' => "15265",
+            'COMMAND_ECHO' => NULL,
+            'COMMAND_HELP' => NULL,
+            'COMMAND_LIST' => NULL,
+            'LANGUAGE_ID' => 'ru',
+            'AUTH' =>
+                array (
+                    'access_token' => '93fe8962005b8db80044c592000035f700000380d6123c7aee51f071f79e7cd863eca8',
+                    'expires' => '1653210771',
+                    'expires_in' => '3600',
+                    'scope' => 'crm,imbot,tasks_extended',
+                    'domain' => 'bitrix.infoflot.com',
+                    'server_endpoint' => 'https://oauth.bitrix.info/rest/',
+                    'status' => 'L',
+                    'client_endpoint' => 'https://bitrix.infoflot.com/rest/',
+                    'member_id' => '20db2a6970cdb3986729fa391fda6f64',
+                    'user_id' => '13815',
+                    'refresh_token' => '837db162005b8db80044c592000035f70000034f6d7ae093ac130e63d4cb0d3eb37bff',
+                    'application_token' => '419f09a694685212cc4a01ff2e629a36',
+                ),
+        ),
+);
+?>
